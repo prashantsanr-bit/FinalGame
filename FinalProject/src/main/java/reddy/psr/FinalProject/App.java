aa61218c2433b5bcc9c234601f29eb5307f5b012
PRASHANT\prash <prashant.reddy@kps.com>
2025-09-20 18:45:27 +0100

    xv

--- FinalProject/src/main/java/reddy/psr/FinalProject/App.java
+++ FinalProject/src/main/java/reddy/psr/FinalProject/App.java
@@ -8,5 +8,6 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("Welcome to GIT practicals");
         System.out.println("UPI Payment implementation");
+        System.out.println("Stashing the changes....");
     }
 }
