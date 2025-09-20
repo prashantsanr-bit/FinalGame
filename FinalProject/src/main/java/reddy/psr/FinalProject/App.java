067c0d24a6c1ca64e34ed57e9e77a59e36c1049e
PRASHANT\prash <prashant.reddy@kps.com>
2025-09-20 18:39:42 +0100

    TL added a comment.

--- FinalProject/src/main/java/reddy/psr/FinalProject/App.java
+++ FinalProject/src/main/java/reddy/psr/FinalProject/App.java
@@ -6,5 +6,6 @@ package reddy.psr.FinalProject;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Welcome to GIT practicals");
     }
 }
